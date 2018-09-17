447c8d679654822841f2b2197aeb0399e43b415f
Gabe D <gpad88@gmail.com>
2018-09-17 12:26:56 -0700

    Manga titles displaying on front page

--- client/src/components/ListContainer.js
+++ client/src/components/ListContainer.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class ListsContainer extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            mangas: []
+        }
+    }
+    componentDidMount() {
+        axios.get('api/v1/mangas.json')
+        .then(response => {
+            console.log(response)
+            this.setState({
+                mangas: response.data
+            })
+        })
+        .catch(error => console.log(error))
+    }
     render() {
         return (
-            <div className="Lists-container">
-                Mangas
+            <div className="lists-container">
+                {this.state.mangas.map( list => {
+                    return (
+                        <div className="single-list" key={list.id}>
+                            <h4>{list.title}</h4>
+                            <p>{list.excerpt}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
 }
-
 export default ListsContainer;
