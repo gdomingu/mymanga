7de38a7c5df0115b745cea71a60a4438c2d2e8ed
Gabe D <gpad88@gmail.com>
2018-09-16 20:17:27 -0700

    Adds list container

---
+++ client/src/components/ListContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListsContainer extends Component {
+    render() {
+        return (
+            <div className="Lists-container">
+                Mangas
+            </div>
+        )
+    }
+}
+
+export default ListsContainer;
